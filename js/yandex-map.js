754eede492eecce35c2d92a8518009e90c2d93b4
Zall <32mx32@gmail.com>
2020-08-10 23:49:59 +0300

    One commit

---
+++ js/yandex-map.js
@@ -0,0 +1,5 @@
+src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af785aea3d376e57765f117390991f6f015bea048d1f1dc6c0a079729a4c0502a&amp";
+width=100%25&amp;
+height=397&amp;
+lang=ru_RU&amp;
+scroll=true;
